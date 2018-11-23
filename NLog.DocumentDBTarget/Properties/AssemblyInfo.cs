5b703d8d5855ef02c6a21139629d845eac7dace9
Joey Frohikey <frohikey@gmail.com>
2018-11-23 09:50:34 +0100

    Version 1.2.0 👽

--- NLog.DocumentDBTarget/Properties/AssemblyInfo.cs
+++ NLog.DocumentDBTarget/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Goto10")]
 [assembly: AssemblyProduct("NLog.DocumentDBTarget")]
-[assembly: AssemblyCopyright("Copyright © 2016 Goto10")]
+[assembly: AssemblyCopyright("Copyright © 2018 Goto10")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
